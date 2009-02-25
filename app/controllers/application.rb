854b6e087ce9a94adc23153bb8a2bdddb6de8f8e
Brian Doll <brian@emphaticsolutions.com>
2009-02-25 12:28:55 -0800

    adding a nice looking UI template

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -3,18 +3,10 @@
 
 class ApplicationController < ActionController::Base
   include AuthenticatedSystem
-  helper :all # include all helpers, all the time
-
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
+  helper :all
   protect_from_forgery # :secret => '6b4eec1d67d3e8ac739030cde388e5f1'
-  
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
-  
-  # used as a filter in controllers
+
+  # Filters:
   def ensure_logged_in
     if (!logged_in?)
       redirect_to login_url
---
+++ config/initializers/cooper_config.rb
@@ -0,0 +1,3 @@
+# Easy app configuration
+SITE_TITLE    = "TJ's Gym - Food Cooperative"
+SITE_SUBTITLE = "Quality food - lower environmental impact - reduced price"
