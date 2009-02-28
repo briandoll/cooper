c0b9bac21e71180725d050dde55b2932a42ad3b4
Brian Doll <brian@emphaticsolutions.com>
2009-02-28 08:04:03 -0800

    changing site title

--- config/initializers/cooper_config.rb
+++ config/initializers/cooper_config.rb
@@ -1,3 +1,3 @@
 # Easy app configuration
-SITE_TITLE    = "TJ's Gym - Food Cooperative"
+SITE_TITLE    = "TJ's Gym @ The Coop Shop"
 SITE_SUBTITLE = "Quality food - lower environmental impact - reduced price"
